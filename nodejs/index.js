c6ca436a7e45ff29b298307071bb5e45e70cbd19
Adriano Machado <adriano.a.machado@accenture.com>
2024-09-05 19:48:56 +0000

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch develop
     Your branch is up to date with 'origin/develop'.
    
     Changes to be committed:
    	modified:   docker-compose.yaml
    	modified:   nodejs/index.html
    	modified:   nodejs/index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -12,40 +12,42 @@ const redisClient3 = redis.createClient({ host: 'keydb_03', port: 6379 });
 // Middleware to parse JSON
 app.use(express.json());
 
-// Endpoint to get the current interval for keydb_01
-app.get('/get-interval', (req, res) => {
-  redisClient1.get('update_interval', (err, interval) => {
-    if (err) {
-      res.status(500).json({ error: 'Error retrieving interval' });
-    } else {
-      res.json({ interval: interval ? parseInt(interval) : 10 });
-    }
-  });
-});
+// Endpoint to change interval for any keydb instance
+app.post('/change-interval/:keydb', (req, res) => {
+  let redisClient;
+  switch (req.params.keydb) {
+    case 'keydb_01':
+      redisClient = redisClient1;
+      break;
+    case 'keydb_02':
+      redisClient = redisClient2;
+      break;
+    case 'keydb_03':
+      redisClient = redisClient3;
+      break;
+    default:
+      return res.status(400).json({ error: 'Invalid KeyDB instance' });
+  }
 
-// Endpoint to handle interval decrease for keydb_01
-app.post('/decrease-interval', (req, res) => {
-  redisClient1.get('update_interval', (err, interval) => {
+  redisClient.get('update_interval', (err, interval) => {
     if (err) {
       res.status(500).json({ error: 'Error retrieving interval' });
     } else {
       let currentInterval = parseInt(interval) || 10;
       let newInterval = currentInterval === 1 ? 10 : currentInterval - 1;
-      redisClient1.set('update_interval', newInterval);
+      redisClient.set('update_interval', newInterval);
       res.json({ newInterval });
     }
   });
 });
 
-// SSE route for real-time updates from keydb_01, keydb_02, and keydb_03
+// SSE route for real-time events from keydb_01, keydb_02, and keydb_03
 app.get('/events', (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache');
   res.setHeader('Connection', 'keep-alive');
 
-  // Function to send updates from all three KeyDB instances
   const sendUpdates = () => {
-    // Get data from keydb_01
     redisClient1.mget(['shared_key', 'update_interval'], (err, values) => {
       if (err) {
         console.error('Error fetching values from keydb_01:', err);
@@ -55,7 +57,6 @@ app.get('/events', (req, res) => {
       }
     });
 
-    // Get data from keydb_02
     redisClient2.mget(['shared_key', 'update_interval'], (err, values) => {
       if (err) {
         console.error('Error fetching values from keydb_02:', err);
@@ -65,7 +66,6 @@ app.get('/events', (req, res) => {
       }
     });
 
-    // Get data from keydb_03
     redisClient3.mget(['shared_key', 'update_interval'], (err, values) => {
       if (err) {
         console.error('Error fetching values from keydb_03:', err);
@@ -76,22 +76,20 @@ app.get('/events', (req, res) => {
     });
   };
 
-  // Send updates every second
   const intervalId = setInterval(sendUpdates, 1000);
 
-  // Clean up when the client disconnects
   req.on('close', () => {
     clearInterval(intervalId);
     res.end();
   });
 });
 
-// Serve the front-end HTML page
+// Basic homepage
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// Start the Express server
+// Start the server
 app.listen(port, () => {
   console.log(`Node.js app listening on port ${port}`);
 });
